a33a28bee9841b66fa60bab666ec0d6103b30fae
wcjameson <williamjameson90@gmail.com>
2022-01-28 10:01:29 -0800

    Correct Pizza constructor test

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Describe: Pizza();
 
-Test: "It should construct a new pizza object with toppings and size and price properties"
-Code: let pizza = new Pizza(["olives", "mushrooms"], "large");
-Expected Output: "{toppings: ["olives", "mushrooms"], size: "large", price: 10 }
+Test: "It should construct a new pizza object with toppings and size properties"
+Code: let pizza = new Pizza("olives", "large");
+Expected Output: "{toppings: "olives", size: "large",}
 
 Describe: calculatePizzaCost();
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-function Pizza(toppings, size, price) {
+function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
-  this.price = 10;
 }
 
 Pizza.prototype.calculatePizzaCost = function() {
-  if (this.toppings === ["cheese"] && this.size === "large") {
-    this.price = 12;
-  } 
+  let price = 10;
+  if (this.toppings === "cheese" && this.size === "large") {
+    price = 12;
+    console.log("your pizza is" + price);
+  } else {
+    console.log("nope");
+  }
 }
 
